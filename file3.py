7df4300a039c3da24a454a585958733bcee9e782
destreza_jay-r <21-73424@g.batstate-u.edu.ph>
2024-07-05 10:30:24 +0800

    Added Rectangle class with area calculation to file3.py.

---
+++ file3.py
@@ -0,0 +1,10 @@
+# file3.py
+class Rectangle:
+ def __init__(self, width, height):
+ self.width = width
+ self.height = height
+
+ def calculate_area(self): 
+ return self.width * self.height
+rectangle = Rectangle(3, 4)
+print(f"Area of the rectangle: {rectangle.calculate_area()}")
